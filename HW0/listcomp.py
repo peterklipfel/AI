6a4981d281c6c41be7a90a5f090713671e6336c4
Peter Klipfel <peter@klipfel.me>
2012-10-13 11:56:54 -0600

    homework 1

--- HW0/listcomp.py
+++
@@ -1,6 +0,0 @@
-nums = [1,2,3,4,5,6]
-plusOneNums = [x+1 for x in nums if x%2 ==1]
-oddNums = [x for x in nums if x%2 ==1]
-print oddNums
-oddNumsPlusOne = [x+1 for x in nums if x%2 == 1]
-print oddNumsPlusOne
